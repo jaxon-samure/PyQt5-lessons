7e4f3668ccdbd5b9b89f27612b0826d8e2c5f3c1
Jaxon <xudoyberdiyevjahon8@gmail.com>
2024-08-24 21:49:26 +0500

    V

--- lesson2/fifteen_games.py
+++ lesson2/fifteen_games.py
@@ -1,10 +1,9 @@
-import os 
-os.system("clear")
-
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QHBoxLayout,QCheckBox
-from PyQt5.QtGui import QIcon
+import random
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
 from PyQt5.QtCore import Qt
+import os 
 
+os.system("clear")
 
 app = QApplication([])
 
@@ -16,64 +15,65 @@ class Window(QWidget):
         self.setStyleSheet("background-color: #F0DBDB;")
         self.resize(500, 330)
         
-       
-        self.btn_coordinates = [[10, 10, 120, 80, "1"], 
-                               [130, 10, 120, 80, "2"], 
-                               [250, 10, 120, 80, "3"], 
-                               [370, 10, 120, 80, "4"], 
-                               [10, 90, 120, 80, "5"],
-                               [130, 90, 120, 80, "6"],
-                               [250, 90, 120, 80, "7"],
-                               [370, 90, 120, 80, "8"],
-                               [10, 170, 120, 80, "9"], 
-                               [130, 170, 120, 80, "10"], 
-                               [250, 170, 120, 80, "11"], 
-                               [370, 170, 120, 80, "12"],
-                               [10, 250, 120, 80, "13"], 
-                               [130, 250, 120, 80, "14"], 
-                               [250, 250, 120, 80, "15"],
-                               ]
-                                
-                            
-                               
-                               
+        self.btn_coordinates = [[10, 10, 120, 80], 
+                                [130, 10, 120, 80], 
+                                [250, 10, 120, 80], 
+                                [370, 10, 120, 80], 
+                                [10, 90, 120, 80],
+                                [130, 90, 120, 80],
+                                [250, 90, 120, 80],
+                                [370, 90, 120, 80],
+                                [10, 170, 120, 80], 
+                                [130, 170, 120, 80], 
+                                [250, 170, 120, 80], 
+                                [370, 170, 120, 80],
+                                [10, 250, 120, 80], 
+                                [130, 250, 120, 80], 
+                                [250, 250, 120, 80 ]]
         
         self.btn_dict = {}
+        self.a = list(random.sample(range(1, 16), k=15))
+        
+        self.move_lst = []
+        x = 0
         for bt in self.btn_coordinates:
-            name = "btn" + bt[4]  
-            self.btn_dict[name] = QPushButton(bt[4], self)
+            name = "btn" + str(self.a[x])
+            self.move_lst.append(self.a[x])  
+            self.btn_dict[name] = QPushButton(str(self.a[x]), self)
             self.btn_dict[name].setGeometry(bt[0], bt[1], bt[2], bt[3])
             self.btn_dict[name].setStyleSheet("""
                             background-color:#222037;
                             color: white;                  
                             """)
-            
-            
-        def find_keys_by_value(d, target_value):
-            return [key for key, value in d.items() if value == target_value]
-        def rm_key(key):
-            for i in self.btn_coordinates:
-                if i[4] == key[4]:
-                    self.btn_coordinates.remove(i)
-                    
-        def bosdi(self):
-            self.btn_coordinates.remove(0)
-            
-            
-            print(self.btn_coordinates)
+            self.btn_dict[name].clicked.connect(self.move_tile)
+            x += 1
+        self.move_lst.append('16')
         
-            
-            
-            
-        self.btn_dict['btn1'].clicked.connect(bosdi)
-
-            
+        self.bosh_btn = [370, 250, 120, 80]
+    def find_key(self, btn):
+        for key, value in self.btn_dict.items():
+            if value == self.clicked_button:
+                return key
+    
+    #win_lst = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15']
+    def move_tile(self):
+        clicked_button = self.sender()
+        clicked_but_geo = clicked_button.geometry()
         
+        if (abs(clicked_but_geo.x() - self.bosh_btn[0]) == 120 and clicked_but_geo.y() == self.bosh_btn[1]):
+            clicked_button.setGeometry(self.bosh_btn[0], self.bosh_btn[1], 120, 80)
+            self.bosh_btn = [clicked_but_geo.x(), clicked_but_geo.y(), 120, 80]
+        if (abs(clicked_but_geo.y() - self.bosh_btn[1]) == 80 and clicked_but_geo.x() == self.bosh_btn[0]):
+            clicked_button.setGeometry(self.bosh_btn[0], self.bosh_btn[1], 120, 80)
+            self.bosh_btn = [clicked_but_geo.x(), clicked_but_geo.y(), 120, 80]
             
-        
-        self.show()
+            
+           
+    
+    
+    
 
 
-
-oyna = Window()  
-app.exec_()  
+window = Window()
+window.show()
+app.exec_()
