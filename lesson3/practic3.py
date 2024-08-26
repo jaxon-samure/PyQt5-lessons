e24ba227d454db2cf418e6992e0b1833a591bde0
Jaxon <xudoyberdiyevjahon8@gmail.com>
2024-08-26 22:15:46 +0500

    add practic

--- lesson3/practic3.py
+++ lesson3/practic3.py
@@ -1,3 +1,34 @@
-import os 
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
+import os
 os.system("clear")
 
+class Oyna(QWidget):
+    def __init__(self) -> None:
+        super().__init__()
+
+        self.setWindowTitle("Nimadir")
+        self.resize(400, 600)
+
+        self.btn = QPushButton("Button", self)
+        self.btn.clicked.connect(self.bos)
+        
+        self.oyna2 = Oyna2()
+        
+        self.show()
+
+    def bos(self):
+        self.oyna2.show()
+        self.close()
+
+class Oyna2(QWidget):
+    def __init__(self) -> None:
+        super().__init__()
+
+        self.setWindowTitle("nima")
+        self.resize(500, 700)
+
+app = QApplication([])
+e = Oyna()
+e.show()
+app.exec_()
